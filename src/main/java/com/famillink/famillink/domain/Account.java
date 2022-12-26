df21edc4b81b5b98ab12ea36962a629d80b9674a
Khan <gjf.kee@gmail.com>
2022-12-26 22:56:31 +0900

    making domain

---
+++ src/main/java/com/famillink/famillink/domain/Account.java
@@ -0,0 +1,47 @@
+package com.famillink.famillink.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerifed;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean familyCreatedByEmail;
+    private boolean familyCreatedByWeb;
+    private boolean familyEnrollmentResultByEmail;
+    private boolean familyEnrollmentResultByWeb;
+    private boolean familyUpdateByEmail;
+    private boolean familyUpdateByWeb;
+}
