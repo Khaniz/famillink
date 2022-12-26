4475234cb5fa486005a0e62ca9268f2a0722f980
Khan <gjf.kee@gmail.com>
2022-12-26 09:49:51 +0900

    Initial commit

---
+++ src/test/java/com/famillink/famillink/FamillinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famillink.famillink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamillinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
