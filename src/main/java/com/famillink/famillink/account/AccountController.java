d286b805a281d46db96fc535483710ddda4f4de2
Khan <gjf.kee@gmail.com>
2022-12-28 23:17:39 +0900

    [회원가입] 파라미터 유효성 검증로직 Annotation

--- src/main/java/com/famillink/famillink/account/AccountController.java
+++ src/main/java/com/famillink/famillink/account/AccountController.java
@@ -2,14 +2,30 @@ package com.famillink.famillink.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
-        model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        
+        // TODO 회원가입처리
+        return "redirect:/";
     }
 }
