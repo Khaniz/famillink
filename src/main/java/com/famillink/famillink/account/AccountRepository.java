20723dd265b5f58daef4f0be228371de6800b3e3
Khan <gjf.kee@gmail.com>
2022-12-29 23:11:35 +0900

    [회원가입] 파라미터 유효성 검증 At Server with InitBinder && WebDataBinder

---
+++ src/main/java/com/famillink/famillink/account/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.famillink.famillink.account;
+
+import com.famillink.famillink.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
