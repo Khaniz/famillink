c7a964f6271f3c4be9de96fdc623f45ddeb4435e
Khan <gjf.kee@gmail.com>
2023-01-09 23:23:18 +0900

    [Annotation] making Annotation via interface AuthenticationPrincipal

---
+++ src/main/java/com/famillink/famillink/main/MainController.java
@@ -0,0 +1,20 @@
+package com.famillink.famillink.main;
+
+import com.famillink.famillink.account.CurrentUser;
+import com.famillink.famillink.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
