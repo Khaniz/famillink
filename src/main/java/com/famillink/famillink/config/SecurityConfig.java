a2b4c66b77873e1449771f4df6bb7f6d6007a579
Khan <gjf.kee@gmail.com>
2023-01-16 23:36:57 +0900

    [쿠키] rememberMe

--- src/main/java/com/famillink/famillink/config/SecurityConfig.java
+++ src/main/java/com/famillink/famillink/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.famillink.famillink.config;
 
+import com.famillink.famillink.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//    private final AccountService accountService;
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Bean
      public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,7 +41,16 @@ public class SecurityConfig {
                 .and()
                 .logout().logoutSuccessUrl("/")
                 .and()
+                .rememberMe().userDetailsService(accountService).tokenRepository(tokenRepository())
+                .and()
                 .build();
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
     @Bean
