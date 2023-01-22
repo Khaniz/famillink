2086f14a89b13ff98ace29ef7c7ddeea83d1ea4d
Khan <gjf.kee@gmail.com>
2023-01-22 21:05:42 +0900

    [테스트코드] 프로필 수정
    -interface Annotation 정의 후
    테스트 진행법
    
    -없으면 만들어 쓰는 방법

--- src/test/java/com/famillink/famillink/settings/SettingsControllerTest.java
+++ src/test/java/com/famillink/famillink/settings/SettingsControllerTest.java
@@ -1,9 +1,11 @@
 package com.famillink.famillink.settings;
 
+import com.famillink.famillink.WithAccount;
 import com.famillink.famillink.account.AccountRepository;
 import com.famillink.famillink.account.AccountService;
 import com.famillink.famillink.account.SignUpForm;
 import com.famillink.famillink.domain.Account;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -29,19 +32,22 @@ class SettingsControllerTest {
     @Autowired
     AccountRepository accountRepository;
 
-    @Autowired
-    AccountService accountService;
-
-    @BeforeEach
-    void beforeEach() {
-        SignUpForm signUpForm = new SignUpForm();
-        signUpForm.setEmail("gjf.kee@gmail.com");
-        signUpForm.setNickname("wisrule");
-        signUpForm.setPassword("dnflEkfchlrh");
-        accountService.processNewAccount(signUpForm);
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
     }
 
-    @WithUserDetails(value = "wisrule", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @WithAccount(value = "wisrule")
+    @DisplayName("프로필 수정하기 - 폼")
+    @Test
+    void updateProfileForm() throws Exception {
+        mockMvc.perform(get(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"));
+    }
+
+    @WithAccount(value = "wisrule")
     @DisplayName("프로필 수정하기 - 정상 입력")
     @Test
     void updateProfile() throws Exception { //setupBefore가 잘 작동되고 있음.
@@ -54,6 +60,28 @@ class SettingsControllerTest {
 
         Account wisrule = accountRepository.findByNickname("wisrule");
         assertEquals(bio, wisrule.getBio());
+
+    }
+
+    @WithAccount(value = "wisrule")
+    @DisplayName("프로필 수정하기 - 정상 입력")
+    @Test
+    void updateProfile_error() throws Exception { //setupBefore가 잘 작동되고 있음.
+        String bio = "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                        .param("bio", bio).with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PROFILE_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors());
+
+        Account wisrule = accountRepository.findByNickname("wisrule");
+        assertNull(wisrule.getBio());
 
     }
 }
