cd065a00e8f674dc2c030cc1c13e9cd1a47e7741
Khan <gjf.kee@gmail.com>
2023-01-23 22:55:54 +0900

    [테스트코드] 프로필 수정

--- src/test/java/com/famillink/famillink/settings/SettingsControllerTest.java
+++ src/test/java/com/famillink/famillink/settings/SettingsControllerTest.java
@@ -4,6 +4,7 @@ import com.famillink.famillink.account.AccountRepository;
 import com.famillink.famillink.account.AccountService;
 import com.famillink.famillink.account.SignUpForm;
 import com.famillink.famillink.domain.Account;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,6 +42,11 @@ class SettingsControllerTest {
         accountService.processNewAccount(signUpForm);
     }
 
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
     @WithUserDetails(value = "wisrule", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @DisplayName("프로필 수정하기 - 정상 입력")
     @Test
@@ -54,6 +60,28 @@ class SettingsControllerTest {
 
         Account wisrule = accountRepository.findByNickname("wisrule");
         assertEquals(bio, wisrule.getBio());
+
+    }
+
+    @WithUserDetails(value = "wisrule", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("프로필 수정하기 - 에러 유발 입력")
+    @Test
+    void updateProfile_error() throws Exception { //setupBefore가 잘 작동되고 있음.
+        String bio = "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우" +
+                "간단 소개 수정이 너무 긴 경우";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                        .param("bio", bio).with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PROFILE_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors());
+
+        Account wisrule = accountRepository.findByNickname("wisrule");
+        assertNull(wisrule.getBio());
 
     }
 }
