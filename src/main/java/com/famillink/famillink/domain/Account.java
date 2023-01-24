11b20dce38db0cf8fc3ba4ba43e75fedd83c2790
Khan <gjf.kee@gmail.com>
2023-01-24 22:37:04 +0900

    [세팅 - 프로필] - 이미지 편집 기능
    직렬화 문제 발생

--- src/main/java/com/famillink/famillink/domain/Account.java
+++ src/main/java/com/famillink/famillink/domain/Account.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @AllArgsConstructor @NoArgsConstructor
-public class Account {
+public class Account{
 
     @Id @GeneratedValue
     private Long id;
