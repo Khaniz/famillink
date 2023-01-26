12e849fd4279cbff2b3a79574c2be435d1e39b16
Khan <gjf.kee@gmail.com>
2023-01-26 22:20:59 +0900

    [테스트코드] 패스워드 변경

--- src/test/java/com/famillink/famillink/settings/SettingsControllerTest.java
+++ src/test/java/com/famillink/famillink/settings/SettingsControllerTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -32,6 +34,9 @@ class SettingsControllerTest {
 
     @Autowired
     AccountService accountService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void beforeEach() {
@@ -82,6 +87,51 @@ class SettingsControllerTest {
 
         Account wisrule = accountRepository.findByNickname("wisrule");
         assertNull(wisrule.getBio());
+
+    }
+
+    @WithUserDetails(value = "wisrule", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    void updatePasswordForm() throws Exception {
+        mockMvc.perform(get(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordDomain"));
+
+    }
+
+    @WithUserDetails(value = "wisrule", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("패스워드 변경 정상")
+    @Test
+    void updatePassword_normal() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                        .param("newPassword", "12345678")
+                        .param("confirmNewPassword", "12345678")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        Account wisrule = accountRepository.findByNickname("wisrule");
+        assertTrue(passwordEncoder.matches("12345678", wisrule.getPassword()));
+    }
+
+    @WithUserDetails(value = "wisrule", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("패스워드 변경 비정상")
+    @Test
+    void updatePassword_abnormal() throws Exception {
+        mockMvc.perform(post(SettingsController.SETTINGS_PASSWORD_URL)
+                        .param("newPassword", "12345678")
+                        .param("confirmNewPassword", "12345679")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("passwordDomain"))
+                .andExpect(model().attributeExists("account"));
+
+
 
     }
 }
